4e3b9f252aaf3280f24fcb19f677fb7d899d4838
derekmoo <138733169+derekmoo@users.noreply.github.com>
2023-08-03 13:45:18 -0400

    Implemented Matilda analysis and figures

--- Analysis.R
+++ Analysis.R
@@ -7,11 +7,11 @@ library(tidyr)
 
 # Configure Hector core
 ini <- system.file("input/hector_ssp245.ini", package = "hector")
-c_ssp245 <- newcore(ini)
+c_ssp245 <- newcore(ini, name = "SSP 2-4.5")
 
 # Generate parameter values and remove ECS
 set.seed(1)
-param_values <- generate_params(c_ssp245, draws = 500)
+param_values <- generate_params(c_ssp245, draws = 15)
 param_values$ECS = NULL
 
 # Parameter histograms
@@ -33,53 +33,97 @@ ggplot(values_long, aes(x = Value, fill = Column)) +
 # Define the different evidence scenarios
 evidence_scenarios <- c("Baseline" = 3.2, "No Process" = 3.3, "UL + EC" = 3.4)
 
-# Generate and store parameter sets for each evidence scenario
-results <- list()
+# Store results for each evidence scenario
+results_list <- list()
+probs_list <- list()
 
 for (scenario in names(evidence_scenarios)) {
+  # Scenario name for storage
+  scenario_name <- paste(scenario, evidence_scenarios[scenario], sep = ", ECS = ")
   
   # Replace the ECS column with the static mean value
   param_values$ECS <- evidence_scenarios[scenario]
   
-  # CALL MATILDA
+  # Running Matilda analysis:
   
-  # results[[scenario]] <- scenario_results
+  # Running Hector iteratively
+  results <-iterate_hector(core = c_ssp245,
+                           params = param_values)
+  
+  # Screening Hector results using score_ramp algorithm
+  scores <- score_hruns(results, criterion_co2_obs(), score_ramp, w1 = 3, w2 = 12)
+  results_scored <- merge(results, scores, by = "run_number")
+  
+  # Store results
+  results_scored$evidence_scenario <- rep(scenario_name)
+  results_list[[scenario]] <- results_scored
+  
+  # Defining and calculating output metrics
+  my_metric <- new_metric(GLOBAL_TAS(), 2000:2100, mean)
+  metric_values <- metric_calc(results, my_metric)
+  
+  # Weighted probabilistic projections
+  bins <- c(0, 1, 1.5, 2, 2.5, 3, 3.5, 4, Inf)
+  probability <- prob_calc(metric_values$metric_result,
+                            bins = bins,
+                            scores = scores$weights)
+  
+  # Store results
+  probabilitydf <- as.data.frame(probability)
+  probabilitydf$evidence_scenario <- rep(scenario_name)
+  probs_list[[scenario]] <- probabilitydf
 }
 
-# # Running Matilda analysis for each line of evidence
-# ######
-# 
-# params <- list_df_params[["Baseline"]]
-# 
-# # Running Hector Iteratively
-# results <-iterate_hector(core = c_ssp245,
-#                          params = params)
-# head(results)
-# 
-# ggplot(results, aes(x = year, y = value, group = run_number, color = as.factor(run_number))) +
-#   geom_line() +
-#   facet_wrap(~variable, scales = "free_y")
-# 
-# # Screening Hector Results
-# scores <- score_hruns(results, criterion_co2_obs(), score_ramp, w1 = 3, w2 = 12)
-# 
-# results_scored <- merge(results, scores, by = "run_number")
-# 
-# ggplot(data = results_scored) +
-#   geom_line(aes(x = year, y = value, group = run_number, color = weights)) +
-#   scale_color_continuous() +
-#   facet_wrap(~variable, scales = "free_y")
-# 
-# # Defining and calculating output metrics
-# 
-# my_metric <- new_metric(GLOBAL_TAS(), 2000:2100, mean)
-# 
-# metric_values <- metric_calc(results, my_metric)
-# 
-# # Weighted probabilistic projections
-# 
-# bins <- c(0, 1, 1.5, 2, 2.5, 3, 3.5, 4, Inf)
-# 
-# prob_calc(metric_values$metric_result,
-#           bins = bins,
-#           scores = scores$weights)
+# Plotting CO2 concentration
+
+results_merge <- do.call(rbind, results_list)
+
+ggplot(subset(results_merge,
+              year > 1990 & year < 2100
+              & variable == CONCENTRATIONS_CO2())) +
+  geom_line(aes(x = year, y = value,
+                group = run_number,
+                color = weights, 
+                alpha = weights),
+            linewidth = 1) +
+  scale_color_gradient(high = "dodgerblue4", low = "lightblue1") +
+  scale_alpha_continuous(range = c(0.1, 1)) +
+  geom_line(data = matilda:::metricdata_co2,
+            aes(year, co2_ppm),
+            color = "red",
+            linewidth = 1) +
+  facet_wrap(~evidence_scenario) +
+  ylab(expression(CO[2]~Concentration~(ppm))) +
+  theme_light() +
+  guides(alpha = "none")
+
+# Plotting probabilities as stacked bar graph
+
+probs_merge <- do.call(rbind, probs_list)
+colnames(probs_merge) <- c("Warming", "Score" ,"Probability", "Scenario")
+
+ggplot(probs_merge, aes(fill = Warming, y = Probability, x = Scenario)) +
+  geom_bar(position = position_fill(reverse = T), 
+           stat = "identity",
+           width = 0.6) +
+  scale_y_continuous(breaks = seq(0, 1.0, 0.1)) +
+  scale_fill_manual(values = c("dodgerblue", 
+                               "skyblue1", 
+                               "mistyrose",
+                               "lightcoral",
+                               "red",
+                               "darkred"),
+                    labels = c("1 to 1.5 C", 
+                               "1.5 to 2 C", 
+                               "2 to 2.5 C", 
+                               "2.5 to 3 C", 
+                               "3.5 to 4 C",
+                               ">4 C")) +
+  coord_flip() +
+  theme_light() 
+
+
+
+
+
+
